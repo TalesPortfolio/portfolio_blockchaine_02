43f349431fef685e284731a169d62712e6189002
tales1982 <142450583+TalesPortfolio@users.noreply.github.com>
2025-01-26 18:24:46 +0100

    Client gerando e recuperando carteiras

--- protochain/src/client/walletClient.ts
+++ protochain/src/client/walletClient.ts
@@ -6,11 +6,113 @@
 /*   By: tales <tales@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 12:00:59 by tales             #+#    #+#             */
-/*   Updated: 2025/01/26 12:19:51 by tales            ###   ########.fr       */
+/*   Updated: 2025/01/26 18:24:01 by tales            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-import Wallet from "../lib/wallet";
+import dotenv from "dotenv";
+dotenv.config();
 
-const wallet = new Wallet("5ee28ff7df74f5720d7a4563f0d44382e6c23e5a4f2240b2386be378d45ff8a9");
-console.log(wallet);
+import axios from "axios";
+import readline from "readline";
+import Wallet from "../lib/wallet";
+import { Console } from "console";
+
+const BLOCKCHAIN_SERVER = process.env.BLOCKCHAIN_SERVER;
+
+let myWalletPub = "";
+let myWalletPriv = "";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function menu() {
+  setTimeout(() => {
+    console.clear();
+    if (myWalletPub) console.log(`You are logged as ${myWalletPub}`);
+    else console.log(`You aren't logged`);
+
+    console.log("1 - Create Wallet");
+    console.log("2 - Recover Wallet");
+    console.log("3 - Balance");
+    console.log("4 - Send tx");
+    rl.question("Choose your option: ", (answer) => {
+      switch (answer) {
+        case "1":
+          createWallet();
+          break;
+        case "2":
+          recoverWallet();
+          break;
+        case "3":
+            getBalance();
+          break;
+        case "4":
+            sendTx();
+          break;
+        default: {
+          console.log("Wrong option");
+          menu();
+        }
+      }
+    });
+  }, 1000);
+}
+
+function preMenu() {
+  rl.question(`Press any key to continue...`, () => {
+    menu();
+  });
+}
+
+function createWallet() {
+  console.clear();
+  const wallet = new Wallet();
+  console.log(`Your new wallet`);
+  console.log(wallet);
+
+  myWalletPub = wallet.publicKey;
+  myWalletPriv = wallet.privateKey;
+  preMenu();
+}
+
+function recoverWallet() {
+  console.clear();
+  rl.question(`What is your private key or WIF?`, (wifOrPrivateKey) => {
+    const wallet = new Wallet(wifOrPrivateKey);
+    console.log(`Your recovered wallet:`);
+    console.log(wallet);
+
+    myWalletPub = wallet.publicKey;
+    myWalletPriv = wallet.privateKey;
+    preMenu();
+  });
+}
+
+function getBalance(){
+    console.clear();
+
+    if (!myWalletPub){
+        console.log(`You don't have a wallet yet.`)
+        return preMenu();
+    }
+
+    //TODO get balance via API
+    preMenu();
+}
+
+function sendTx(){
+    console.clear();
+
+    if (!myWalletPub){
+        console.log(`You don't have a wallet yet.`)
+        return preMenu();
+    }
+
+    //TODO send TX via API
+    preMenu();
+}
+
+menu();
