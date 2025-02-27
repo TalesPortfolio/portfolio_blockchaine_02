f38b777dc59dd2a4cdd40b932a1ae91d4cfc261e
tales1982 <142450583+TalesPortfolio@users.noreply.github.com>
2025-02-27 19:31:05 +0100

    Protominer com UTXO

--- protochain/src/lib/block.ts
+++ protochain/src/lib/block.ts
@@ -69,7 +69,7 @@ export default class Block{
      * @returns returns if the block is valid
      */
     
-    isValid(previousHash: string, previousIndex: number, difficult: number,):Validation{
+    isValid(previousHash: string, previousIndex: number, difficult: number, feePerTx: number):Validation{
         
         if(this.transactions && this.transactions.length){
             const feeTxs = this.transactions.filter(tx => tx.type === TransactionType.FEE );
@@ -82,8 +82,8 @@ export default class Block{
             if(!feeTxs[0].txOutputs.some(txo => txo.toAddress === this.miner))
                 return new Validation(false, "Invalid fee tx: different from miner.")
 
-            //TODO: colocar validacao de quantidade de taxas
-            const validations = this.transactions.map(tx => tx.isValid());
+            const totalFees = feePerTx * this.transactions.filter(tx => tx.type !== TransactionType.FEE).length;
+            const validations = this.transactions.map(tx => tx.isValid(difficult, totalFees));
             const errors = validations.filter(v => !v.success).map(v => v.message);
             if(errors.length > 0)
                 return new Validation(false, "Invalid block due to invalid tx: " + errors.reduce((a, b) => a + b));
